70857cc0544fde46fc4c3383deaae3cf23386a05
jsw0115 <jsw0101151@naver.com>
2025-11-29 17:36:15 +0900

    react 환경 구성

---
+++ src/test/java/com/timepalette/daylogue/DaylogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timepalette.daylogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaylogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
